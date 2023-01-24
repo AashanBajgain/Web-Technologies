b444d30c0a6d0dc6a5245e17d2687344a22f71fe
AashanBajgain <119468487+AashanBajgain@users.noreply.github.com>
2023-01-24 13:07:37 +0545

    created the first basic approach for website

---
+++ Week 9/JS/Javascript.js
@@ -0,0 +1,18 @@
+alert("Don't halla in class.");
+console.log("hello from the other side......")
+console.log(document.getElementById("Paragraph").innerHTML)
+document.getElementById("Paragraph").innerHTML="<h1>Hello World<h1>";
+/* document.getElementById("Paragraph").style.color="purple";
+document.getElementById("Paragraph").style.backgroundColor="biseque" */
+document.getElementById("Paragraph").style.cssText="color:purple; background:aqua"; 
+var age=21;
+age=13;
+const age1=22;
+/* age1=12 */
+let age3=23;
+age3=25
+var name="Aashan";
+console.log(age);
+console.log(age1);
+console.log(typeof age3);
+console.log(typeof name);
